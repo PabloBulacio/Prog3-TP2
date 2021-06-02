25ae3122a4a2f9ec1c36f31064b335c328152779
Pablo Torres <p.torres0991@gmail.com>
2021-06-02 00:44:07 -0300

    Agregando referencias, formulario de productos, formulario de error. Funcionalidad para que en formulario de Productos se carguen articulos.

---
+++ WebApp/Pokemones.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pokemones.aspx.cs" Inherits="WebApp.Pokemones" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:GridView ID="dgvProducts" runat="server"></asp:GridView>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/Pokemones.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+
+namespace WebApp
+{
+    public partial class Pokemones : System.Web.UI.Page
+    {
+        private List<Articulos> listaArticulos;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ArticulosNegocio articulosNegocio = new ArticulosNegocio();
+
+            try
+            {
+                listaArticulos = articulosNegocio.Listar();
+                dgvProducts.DataSource = listaArticulos;
+                dgvProducts.DataBind();
+            }
+            catch(Exception ex)
+            {
+                Response.Redirect("Error.aspx");
+            }
+        }
+    }
+}
